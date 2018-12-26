56581b75a35972aa13ea1a01f36528f96c603179
Spencer Arnold <sa.variable@gmail.com>
2018-12-25 21:13:29 -0600

    Populated general code
    created entry point, prompt, and sendReq function

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+#WikiLinkMapper v1
+#Spencer Arnold
 
+import urllib.request
+
+#Entry 
+def main():
+    root = prompt()
+    contents = str(urllib.request.urlopen(root).read())
+    
+
+def prompt():
+    
+    #root page
+    return root = input("Please enter root wiki page link: ")
+    
+    #simplify entry mechanism by abstracting link entry away.
+    #this will be more complex than you think.
+    #Hence the prompt func.
+
+def sendReq(url):
+    
+    return str(urllib.request.urlopen(root).read())
+    
+if __name__ == '__main__':
+    main()
