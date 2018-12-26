99402a67e264a81790abad204ac526685e701624
Spencer Arnold <sa.variable@gmail.com>
2018-12-25 21:31:59 -0600

    Built out parseRaw base

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import urllib.request
 
 #Entry 
 def main():
-    root = prompt()
-    contents = str(urllib.request.urlopen(root).read())
+    #This is about to be so bad.
+    parseRaw(sendReq(prompt()))
     
 
 def prompt():
@@ -21,6 +21,17 @@ def prompt():
 def sendReq(url):
     
     return str(urllib.request.urlopen(root).read())
+
+def parseRaw(rawBody):
+    recognizer = 0
+    for charac in rawBody:
+        if (charac == 'h' and recognizer == 0) or (charac == 'r' and recognizer == 1) or (charac == 'e' and recognizer == 2) || (charac == 'f' and recognizer == 3):
+            recognizer++
+            if(recognizer == 4):
+                print("Found URL")
+        else
+            recognizer = 0
+        
     
 if __name__ == '__main__':
     main()
