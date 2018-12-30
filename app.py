5ffaa6cebe6ee5806b4dfa8b6b8d4d61a53b6719
Spencer Arnold <sa.variable@gmail.com>
2018-12-30 01:44:15 -0600

    Got parser working and identifying links

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ import urllib.request
 #Entry 
 def main():
     #This is about to be so bad.
-    parseRaw(sendReq(prompt()))
+    
+    contents = sendReq(prompt())
+    with open("raw.txt" , "w") as raw:
+        raw.write(contents)
+        
+    parseRaw()
     
 
 def prompt():
@@ -15,15 +20,40 @@ def prompt():
     return input("Please enter root wiki page link: ")
     
     #simplify entry mechanism by abstracting link entry away.
-    #this will be more complex than you think.
     #Hence the prompt func.
 
 def sendReq(root):
     
     return str(urllib.request.urlopen(root).read())
 
-def parseRaw(haystack):
-   
+def parseRaw():
+    #make algorithm cleaner. perhaps general. pass in string and loop through each character for matching or something.
+    with open("raw.txt", 'r') as raw:
+        with open("Bare_Urls.txt" , 'w') as bare:
+            
+            for char in iter(lambda: raw.read(1), ''):
+                #print("here")
+                if char == 'h':
+                    
+                    char = raw.read(1)
+                    if char == 'r':
+                        char = raw.read(1)
+                        if char == 'e':
+                            char = raw.read(1)
+                            if char == 'f':
+                                raw.read(2)
+                                char = raw.read(1)
+                                url = ""
+                                #print("Found a URL in Response Data!")
+                                while char != '"':
+                                    url += char
+                                    char = raw.read(1)
+                                    #cleans up non wiki related links
+                                if "wiki" in url:
+                                    url += '\n'
+                                    bare.write(url)
+                        
+                    
         
     
 if __name__ == '__main__':
